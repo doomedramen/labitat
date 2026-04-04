179dfe112a66dc0e2abbe9249c7242c597231543
Martin Page <wookoouk@gmail.com>
2026-04-04 18:21:46 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,7 +55,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "node scripts/clean-test-db.mjs && pnpm db:push && pnpm dev",
+    command:
+      "node scripts/clean-test-db.mjs && node scripts/build-sw.js && pnpm db:push && pnpm dev",
     url: "http://localhost:3000",
     reuseExistingServer: false,
     timeout: 120 * 1000,
